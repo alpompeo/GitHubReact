805e473ca3c1b4f0cc8dd0f7ec2e772cf0962de2
alpompeo <andrepompeo@gmail.com>
2020-02-16 18:42:23 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export const api = axios.create({
   baseURL: 'https://api.github.com/',
   headers: {
-    Authorization: `bearer ${'b24b9b8efc697b22b0d17c8baf2af8453cdb4534'}`,
+    Authorization: `bearer ${'your token'}`,
   },
 });
